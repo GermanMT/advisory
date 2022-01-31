e5e809f1c9032e0c5c996d4ebecc4961f0a8270f
GermanMT <germanoctako@gmail.com>
2022-01-31 01:50:22 +0100

    feat: add pysmt funcionality
    Added pysmt funcionality in the metamodel class provisionally.
    [A

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from metamodel.metamodel import metamodel
 
-metamodel = metamodel(['setup.py'])
+metamodel = metamodel(['setup.py'], 'psf/requests')
 
 metamodel.generate_metamodel()
--- metamodel/metamodel.py
+++ metamodel/metamodel.py
@@ -1,81 +1,97 @@
-from metamodel.dependencies import dependencies
-from metamodel.versions import get_versions
+from metamodel.raw_data import raw_data
 
-import operator
+from pysmt.shortcuts import  Equals, GT, LT, GE, LE, NotEquals, Symbol, And, Or, EqualsOrIff, Int, get_model, Solver, Not
+from pysmt.typing import INT
+from pysmt.oracles import get_logic
 
 
 class metamodel():
 
-    def __init__(self, files: list[str]) -> None:
+    def __init__(self, files: list[str], nameWithOwner: str) -> None:
         self.files = files
-        self.dependencies = dependencies('')
+        self.nameWithOwner = nameWithOwner
+        self.raw_data = raw_data()
         self.ops = {
-            '=': operator.eq,
-            '>': operator.gt,
-            '<': operator.lt,
-            '>=': operator.ge,
-            '<=': operator.le,
-            '!=': operator.ne,
-            '~>': self.approx_gt
+            '=': Equals,
+            '>': GT,
+            '<': LT,
+            '>=': GE,
+            '<=': LE,
+            '!=': NotEquals,
+            '~>': GE
             }
 
     ''' Una vez obtengamos el dicionario con las dependencias asociadas a las distribuciones
     permitidas por el formato de la versión comenzaremos a construir el metamodelo'''
     def generate_metamodel(self) -> None:
         for file in self.files:
-            print(self.get_raw(file))
+            raw_data = self.raw_data.get_data(file, self.nameWithOwner)
+            ''' Añadir variables y restricciones al smt '''
+            domains = []
+            vars_ = []
+            for variable in raw_data:
+                var = Symbol(variable, INT)
+                vars_.append(var)
 
-    ''' Usar metodo para recoger y comparar versiones obtenidas en pypi con el formato
-    obtenido, y así construir el diccionario de dependencias asocidas a las distribuciones 
-    aceptadas '''
-    def get_raw(self, file: str) -> dict[str, list[str]]:
-        self.dependencies.file_type = file
+                if variable in self.raw_data.problems:
+                    p_domain = self.add_problems(var, self.raw_data.problems[variable])
+                v_domain = Or([Equals(var, Int(self.transform(version))) for version in raw_data[variable]])
+                
+                aux = [v_domain]
+                aux.extend(p_domain)
+                domains.append(And(aux))
 
-        # GermanMT/AMADEUS
-        # psf/requests
-        dependencies = self.dependencies.get_dependencies('psf/requests')
 
-        relationships = {}
+            # print(domains)
 
-        for pkg_name in dependencies:
-            if dependencies[pkg_name] == 'Any':
-                distributions = get_versions(pkg_name)
-                relationships[pkg_name] = distributions
-            else:
-                parts = dependencies[pkg_name].split(',')
-                constraints = self.get_constraints(parts)
-                distributions = self.get_distributions(pkg_name, constraints)
-                relationships[pkg_name] = distributions
+            ''' Muestra y analiza todas las soluciones '''
+            self.all_smt(And(domains), vars_)
 
-        return relationships
+            # for domain in domains:
+            #     print(domain)
 
-    @staticmethod
-    def approx_gt(version: str, version_: str) -> bool:
-        tam = len(version_) - 1
-        return version >= version_ and version[tam] >= version_[tam]
+            #     ''' Consigue una única solución satisfacible para cada dominio '''
+            #     model = get_model(domain)
+            #     print(model)
 
     @staticmethod
-    def get_constraints(parts: list[str]) -> dict[str, str]:
-        constraints = {}
+    def all_smt(formula, keys: list[Symbol]) -> None:
+        target_logic = get_logic(formula)
+        print("Target Logic: %s" % target_logic)
+        with Solver(logic=target_logic) as solver:
+            solver.add_assertion(formula)
+            while solver.solve():
+                partial_model = [EqualsOrIff(k, solver.get_value(k)) for k in keys]
+                print(partial_model)
+                solver.add_assertion(Not(And(partial_model)))
 
-        for part in parts:
-            attr = part.split(' ')
-            if part.__contains__('||'):
+    ''' Mejorar método para asignar a las versiones una combinación única'''
+    @staticmethod
+    def transform(version: str) -> int:
+        ''' Si no está completa se añade un '.0.0' / '.0' al final de la version '''
+        dots = version.count('.')
+        if dots == 1:
+            version = version + '.0'
+        elif dots == 0:
+            version = version + '.0.0'
+
+        l = [int(x, 10) for x in version.split('.') if x.isnumeric()]
+        l.reverse()
+        version = sum(x * (100 ** i) for i, x in enumerate(l))
+        return version
+
+    ''' Crea las restricciones para el modelo smt '''
+    def add_problems(self, var: Symbol, problems: dict[str, str]) -> list:
+        problems_ = []
+
+        for problem in problems:
+            if problem.__contains__('||'):
                 op = '!='
-                version_ = attr[1]
+                version_ = problems[problem]
             else:
-                op = attr[0]
-                version_ = attr[1]
-            constraints[op] = version_
+                op = problem
+                version_ = problems[problem]
+            problem_ = self.ops[op](var, Int(self.transform(version_)))
+            problems_.append(problem_)
 
-        return constraints
-
-    def get_distributions(self, pkg_name: str, constraints: dict[str, str]) -> list[str]:
-        distributions = []
-
-        for version in get_versions(pkg_name):
-            checkers = [self.ops[op](version, constraints[op]) for op in constraints]
-            if all(checkers):
-                distributions.append(version)
-
-        return distributions
+        return problems_
