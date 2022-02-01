4a736c45f6cba674dcda9edf5417588b71f48cf0
GermanMT <germanoctako@gmail.com>
2022-02-01 12:46:47 +0100

    feat: use z3 solver
    Started to use Z3 solver instead MSAT solver.

--- metamodel/metamodel.py
+++ metamodel/metamodel.py
@@ -49,12 +49,12 @@ class metamodel():
     def all_smt(formula, keys: list[Symbol]) -> None:
         i = 0
         target_logic = get_logic(formula)
-        with Solver(logic=target_logic) as solver:
+        with Solver(logic = target_logic, name = 'z3') as solver:
             solver.add_assertion(formula)
             while solver.solve():
                 partial_model = [EqualsOrIff(k, solver.get_value(k)) for k in keys]
                 i += 1
-                # print(i)
+                print(i)
                 # print(partial_model)
                 solver.add_assertion(Not(And(partial_model)))
 
