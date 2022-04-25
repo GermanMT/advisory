68eab67705f3fb8f7969c8a313b2466a6963fffb
GermanMT <germanoctako@gmail.com>
2022-04-25 15:27:41 +0200

    feat: Improve dependencies extraction

--- graph/apis/git/dependencies.py
+++ graph/apis/git/dependencies.py
@@ -1,40 +1,59 @@
-from requests import request
+import requests
 
-from graph.apis.git.req_files import get_req_files
-
+from graph.objects.model.package import Package
 
 headers = {
     'Accept': 'application/vnd.github.hawkgirl-preview+json',
-    'Authorization': 'Bearer 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',
+    'Authorization': 'Bearer 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',
 }
 
-url = 'https://api.github.com/graphql'
+endpoint = 'https://api.github.com/graphql'
 
-def get_dependencies(name_with_owner: str, pkg_manager: str) -> dict[str, str]:
-    atts = name_with_owner.split('/')
-    query = '{\"query\":\"query {\\n repository(owner:\\\"' \
-        + atts[0] + '\\\", name:\\\"' \
-        + atts[1] + '\\\") {\\n dependencyGraphManifests { \\n edges ' \
-        '{ \\n node { \\n blobPath \\n dependencies { \\n nodes ' \
-        '{ \\n repository { \\n nameWithOwner \\n } \\n packageName ' \
-        '\\n requirements \\n hasDependencies \\n packageManager \\n ' \
-        '} \\n } \\n } \\n } \\n } \\n } \\n } \" }'
+def get_dependencies(parent: Package) -> dict[str, str]:
+    owner, name = parent.name_with_owner.split('/')
+    query = '''{
+            repository(owner: \"%s\", name: \"%s\")
+            {
+                dependencyGraphManifests
+                {
+                    nodes
+                    {
+                        filename
+                        dependencies
+                        {
+                            nodes
+                            {
+                                packageName
+                                requirements
+                                hasDependencies
+                                repository
+                                {
+                                    nameWithOwner
+                                }
+                                packageManager
+                            }
+                        }
+                    }
+                }
+            }
+        }''' % (owner, name)
 
-    response = request('POST', url, data = query, headers = headers)
-    return json_reader(response.json(), pkg_manager)
+    response = requests.post(endpoint, json={"query": query}, headers = headers)
 
-def json_reader(data, pkg_manager: str) -> dict[str, str]: 
+    return json_reader(response.json(), parent)
+
+def json_reader(data, parent: Package) -> dict[str, str]: 
     dependencies = dict()
 
-    for edge in data['data']['repository']['dependencyGraphManifests']['edges']:
-        file = edge['node']['blobPath'].split('/')[-1]
-        for node in edge['node']['dependencies']['nodes']:
-            if node['repository'] != None and node['packageManager'] == pkg_manager:
+    for node in data['data']['repository']['dependencyGraphManifests']['nodes']:
+        file = node['filename']
+        parent.req_files.append(file)
+        for node in node['dependencies']['nodes']:
+            if node['repository'] != None and node['packageManager'] == parent.pkg_manager:
                 package_manager = node['packageManager']
                 has_dependencies = node['hasDependencies']
                 name_with_owner = node['repository']['nameWithOwner']
-                req_files = get_req_files(name_with_owner)
                 req = node['requirements'] if node['requirements'] else 'Any'
-                dependencies[node['packageName']] = [package_manager, file, has_dependencies, name_with_owner, req_files, req]
+                dependencies[node['packageName']] = [package_manager, file, has_dependencies, name_with_owner, req]
 
     return dependencies
